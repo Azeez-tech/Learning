7313dceba321da601ee869599697e55b516c411f
Azeez Muhammed <muhammedazeez09@gmail.com>
2024-09-01 09:54:43 -0400

    initial commit

---
+++ javacript.js
@@ -0,0 +1,191 @@
+// this is an undefined way of declaring variables.
+//let firstname;
+/* This will display in the browser console and gives you
+an undefined value, showing that the above declaration is not assigned any value. */
+/*console.log(firstname)
+let a = 10
+let b = 15
+alert(a + b)
+console.log(a > b)
+console.log(typeof false)
+console.log(typeof 25)*/
+
+/*
+let firstName = "Azeez"
+let lastName = "Muhammed"
+let marritalStatus = "Married"
+let country = "Nigeria"
+let age = 41
+let myAge = 41
+let yourAge = 34
+console.log(myAge, yourAge)
+let numOne = 3
+let numTwo = 3
+console.log(numOne == numTwo)
+let js = 'javascript'
+let py = 'python'
+console.log(js == py)
+let lightOn = true
+let lightOff = false
+console.log(lightOn == lightOff)*/
+
+/* non primitive datatypes are mutable and can be modified. eg Arrays, Object'
+An Array is a list of data values in a square bracket */
+/* let nums = [1, 2, 3]
+nums[10]
+console.log(nums) */
+
+/*let nums = [1, 2, 3]
+let numbers = [1, 2, 3]
+console.log(nums == numbers)
+
+
+let userOne = {
+    name: "Azeez",
+    role: "Learning",
+    country: "Nigeria"
+}
+
+let userTwo = {
+    name: "Azeez",
+    role: "Learning",
+    country: "Nigeria"
+}
+
+console.log(userOne == userTwo)*/
+
+/*let nums = [1, 2, 3]
+let numbers = nums
+console.log(nums == numbers)
+
+let userOne = {
+    name: "Azeez",
+    role: "Learning",
+    country: "Nigeria"
+}
+console.log(userOne)
+let userTwo = userOne
+console.log(userOne == userTwo)
+const PI = Math.PI
+console.log(PI)
+// concatenation adding or merging two or strings together.
+
+let firstName = "Azeez"
+let lastName = "Muhammed"
+let fullName = firstName + " " + lastName
+let country = "Nigeria"
+let age = 41
+let city = "Ilorin"
+let personalinfo = ` My name is ${fullName} \n I am ${41} years old \n I am a ${country} \n I lve in ${city}.`
+alert(personalinfo)
+
+let js = "javascript"
+console.log(js.length)
+let string = "javascript"
+let firstLetter = string[0]
+console.log(firstLetter)
+
+let lastLetter = string[9]
+console.log(lastLetter)
+console.log(string.toUpperCase())
+console.log(js.toLowerCase())
+
+let challenge = "30 Days Of Javascript"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+
+const boilingPoint = 100
+const bodyTemp = 36
+console.log(`Boiling point of water is ${boilingPoint}oC.\n Body temperature is ${bodyTemp}oC.`)
+
+let number = prompt(`Enter number`)
+console.log(number)
+const agree = confirm(`Are you sure you want to delete this?`)
+console.log(agree)*/
+
+// time Object
+
+/*let now = new Date()
+console.log(now)
+
+// to other full year
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDay())
+console.log(now.getTime())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+
+
+// conditional statement using if, if else,if else if else, switch, ternary operator
+
+let num = 3
+if (num > 0) {
+    console.log(`${num} is a positive integer`)
+}
+
+let isRaining = true
+if (isRaining) {
+    console.log(`remember to take your rain coat.`)
+}
+
+let num = 3
+if (num > 0){
+    console.log(`${num} is a positive number`)
+} else {
+    console.log(`${num} is a negative number`)
+}
+
+let numb = -1
+if (numb > 2){
+    console.log(`${numb} is a positvie number`)
+} else {
+    console.log(`${numb} is a negative number`)
+}
+
+
+let isRaining = true
+if (isRaining){
+    console.log(`Remember to take your rain coat`)
+} else {
+    console.log(`no need to take a rain coat.`)
+}*/
+
+
+/*let  isRaining = false
+if (isRaining){
+    console.log(`Remember to take your rain coat.`)
+} else {
+    console.log(`No need to take your rain coat`)
+}
+
+
+let a = 6
+if (a < 3){
+    console.log(`${a} is small'`)
+} else if (a == 0){
+    console.log(`${a} is smaller`)
+} else if (a > 5){
+    console.log(`${a} is bigger`)
+} else {
+    console.log(`${a} is zero`)
+}
+
+
+let weather = "cloudy"
+switch (weather) {
+    case `rainy`:
+        console.log(`you need a rain coat`)
+        break;
+    case `cold`:
+       console.log(`It might be cold, you need a jacket`)
+        break;
+    case `cloudy`:
+        console.log(`Go out free`)
+         break;
+    default:
+        console.log(`No need for rain coat.`)
+}*/
